052e6fa18c1f34c690870f700fe537e20b0678be
raialucas1 <raialucas@hotmail.com>
2024-04-02 18:46:54 -0500

    subtotal e total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -58,7 +58,9 @@ public class OrderItem implements Serializable{
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
+    public Double getSubTotal() {
+    	return quantity*price;
+    }
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
